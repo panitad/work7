44f756f8b9b28b4788a4ec79ea835924835954a9
panitad <139569094+panitad@users.noreply.github.com>
2024-02-24 23:31:23 -0800

    Create w3.py

---
+++ w3.py
@@ -0,0 +1,8 @@
+tuser = "admin"
+tpassword = "Ad13n@23t"
+user = input("User: ")
+password = input("Password: ")
+if user == tuser and password == tpassword:
+    print("เข้าสู่ระบบสำเร็จ")
+else:
+    print("เข้าสู่ระบบไม่สำเร็จ")
